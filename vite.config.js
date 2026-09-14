89efaf898cbb9a37d05d956fc637f8d187e20067
jeh993 <jeff@adrenalinegarage.com>
2026-09-13 23:37:13 -0600

    Updated vite so that it points to the right palce in dist.

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig(({ command }) => ({
-    base:
-        command === "build"
-            ? "/eve-frontier-vylent-free-stuff-dapp/dist/"
-            : "/",
+    base: "./",
 
     build: {
         rollupOptions: {
@@ -14,24 +11,6 @@ export default defineConfig(({ command }) => ({
                 admin: resolve(__dirname, "admin.html"),
                 eveAdmin: resolve(__dirname, "eve-admin.html"),
                 eveDeposit: resolve(__dirname, "eve-deposit.html"),
-            },
-        },
-    },
-
-    server: {
-        proxy: {
-            "/sui-rpc": {
-                target: "https://fullnode.testnet.sui.io",
-                changeOrigin: true,
-                secure: true,
-                rewrite: (path) => path.replace(/^\/sui-rpc/, ""),
-            },
-
-            "/sui-mainnet-rpc": {
-                target: "https://fullnode.mainnet.sui.io",
-                changeOrigin: true,
-                secure: true,
-                rewrite: (path) => path.replace(/^\/sui-mainnet-rpc/, ""),
             },
         },
     },
